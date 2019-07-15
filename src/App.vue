3f186b91a16bf33c2b353322d4a05c5734fd853a
Grant James <Grant.James@bcu.ac.uk>
2019-07-15 13:21:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <search-box @searched="searched"/>
+
+        <p><span v-show="searchTerm">You searched for: </span>{{ searchTerm }}</p>
+    </div>
+</template>
+
+<script>
+import SearchBox from './components/SearchBox.vue'
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            searchTerm: ""
+        }
+    },
+    components: {
+        SearchBox
+    },
+    methods: {
+        searched(searchTerm) {
+            this.searchTerm = searchTerm;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav class="level">
+        <div class="level-left">
+            <div class="level-item">
+                <p class="subtitle is-5">
+                    <strong>Azure Search</strong> Vue Demo
+                </p>
+            </div>
+            <div class="level-item">
+                <div class="field has-addons">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Film name or actor" v-model="searchTerm">
+                    </div>
+                    <div class="control">
+                        <a class="button is-info" @click="search">
+                            Search
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+    
+</template>
+
+<script>
+export default {
+    name: 'SearchBox',
+    data() {
+        return {
+            searchTerm: ""
+        }
+    },
+    methods: {
+        search() {
+            this.$emit('searched', this.searchTerm);
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
